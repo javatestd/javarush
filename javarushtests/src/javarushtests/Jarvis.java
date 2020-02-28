516e408a411086bf0c4a10a4f58bd6b7ca7c6b92
lenevo <lenevo@158.36.59.154>
2020-02-28 16:31:18 +0100

    First Comit

---
+++ javarushtests/src/javarushtests/Jarvis.java
@@ -0,0 +1,25 @@
+package javarushtests;
+
+public class Jarvis {
+
+	/**
+	 * @param name
+	 */
+	public void sayHi(String firstguest) {
+
+		System.out.println("Hi mr. " + firstguest + ", how are you?");
+
+	}
+
+	public void sayHi(String firstguest, String secondguest) {
+		System.out.println("Hi " + firstguest +" and "+ secondguest +", how are you?");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Jarvis jarvis = new Jarvis();
+		jarvis.sayHi("Tom");
+        jarvis.sayHi("Tom", "Monica");
+	}
+}
